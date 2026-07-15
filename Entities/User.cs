8c00f4bdeba398425d8993f40d777ea426703e12
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-07-15 17:17:59 +0330

    feat: Add password hash to user entity

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,6 +4,7 @@ public class User
 {
     public int Id { get; set; }
     public string Username { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
     public DateTime CreatedAt { get; set; }
     public ICollection<Pixel> Pixels { get; set; } = new List<Pixel>();
 }
