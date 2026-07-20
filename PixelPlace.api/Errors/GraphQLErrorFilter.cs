2b848374a10afb1374e6d9bbc83bc53c9959252f
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-07-20 18:47:48 +0330

    feat: Add unauthorized error handling in filter error

--- PixelPlace.api/Errors/GraphQLErrorFilter.cs
+++ PixelPlace.api/Errors/GraphQLErrorFilter.cs
@@ -6,6 +6,13 @@ public class GraphQLErrorFilter : IErrorFilter
 {
     public IError OnError(IError error)
     {
+        if (error.Code == "AUTH_NOT_AUTHORIZED")
+        {
+            return error
+                .WithMessage("You must be logged in to perform this action.")
+                .WithCode("UNAUTHORIZED");
+        }
+
         if (error.Exception is not null)
         {
             return error
